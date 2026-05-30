4027f92042b1cd4fe6e5a6beacc7f0f26a43a2b1
Vagner Goncalves <vlgonalves@gmail.com>
2026-05-29 22:08:29 -0300

    feat(service.product): init spring-boot product

---
+++ services/product/src/test/java/com/github/vagnerlg/product/ProductApplicationIT.java
@@ -0,0 +1,14 @@
+package com.github.vagnerlg.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import(TestcontainersConfiguration.class)
+class ProductApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+}
