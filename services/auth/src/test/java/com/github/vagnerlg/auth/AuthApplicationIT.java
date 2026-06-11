96c58cc03c674e12cebf3fce9f1e0d23ad586643
Vagner Goncalves <vlgonalves@gmail.com>
2026-06-11 19:46:55 -0300

    feat(auth): init auth service spring boot

---
+++ services/auth/src/test/java/com/github/vagnerlg/auth/AuthApplicationIT.java
@@ -0,0 +1,15 @@
+package com.github.vagnerlg.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import(TestcontainersConfiguration.class)
+class AuthApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
